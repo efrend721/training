9eba0bb3cf589cd5ea07e4c9f7aba5286e4c1a9a
Ervis <efrend721@gmail.com>
2025-07-27 05:11:00 -0600

    adding first useMemo, useCallback and useRef hooks example.

--- react/vanilla-projects/useSomething/src/App.jsx
+++ react/vanilla-projects/useSomething/src/App.jsx
@@ -1,33 +1,41 @@
-import { useState, useEffect } from "react";
-
+import { useState, useMemo,  } from "react";
 import "./App.css";
+import { useCallback } from "react";
 
 function App() {
   const [name, setName] = useState("Ervis");
-  const refButton = useRef(null);
+  //const refButton = useRef(null);
 
-  useEffect(() => {
+  /*useEffect(() => {
     console.log("rendered");
     return () => {};
   }, [name]);
+  */
 
-  const handleClick = () => {
+  const changedName = useMemo(() => {
+    return `Hola ${name}`;
+  }, [name]);
+
+
+  const handleClick = useCallback(() => {
     setName("Pedro");
-  }
+  }, [name]);
 
   return (
     <div className="App">
-      <h1>
-        Hola, {name}!
-      </h1>
-      <button ref={refButton} onClick={handleClick}>
+      <h1>Hola, {name}!</h1>
+      <ChangeName changeName={handleClick} />
+    </div>
+  );
+}
+
+function ChangeName ({changeName}){
+  
+  return (
+    <div>
+      <button onClick={changeName}>
         Change name
       </button>
-      <button onClick={() => refButton.current.click()}>
-        click the other button to change name
-      </button>
-    
-    
     </div>
   );
 }
