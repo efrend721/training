2d8a0cd98e0d8064c53d28d316370ce5f59168da
Ervis <efrend721@gmail.com>
2025-08-07 22:13:49 -0600

    add role-based access control

--- react/vanilla-projects/ervis-router/src/App.tsx
+++ react/vanilla-projects/ervis-router/src/App.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Navigate, Route } from "react-router-dom";
 import "./App.css";
 import { PrivateRoutes, PublicRoutes } from "./models";
-import { AuthGuard } from "./guards";
+import { AuthGuard, RolGuard } from "./guards";
 import { RoutesWithNotFound } from "./utilities";
 import { Suspense, lazy } from "react";
 import { Provider } from "react-redux";
 import store from "./redux/store";
 import { Logout } from "./components/Logout";
+import { Roles } from "./models/roles";
+import Dashboard from "./pages/Private/Private";
 
 const Login = lazy(() => import("./pages/Login/Login"));
 const Private = lazy(() => import("./pages/Private/Private"));
@@ -24,11 +26,14 @@ function App() {
                 element={<Navigate to={PrivateRoutes.PRIVATE} />}
               />
               <Route path={PublicRoutes.LOGIN} element={<Login />} />
-              <Route element={<AuthGuard />}>
+              <Route element={<AuthGuard privateValidation = {true} />}>
                 <Route
                   path={`${PrivateRoutes.PRIVATE}/*`}
                   element={<Private />}
                 />
+              </Route>
+              <Route element={<RolGuard rol={Roles.ADMIN} />}>
+                <Route path={PrivateRoutes.DASHBOARD} element={<Dashboard />} />
               </Route>
             </RoutesWithNotFound>
           </BrowserRouter>
--- react/vanilla-projects/ervis-router/src/guards/auth.guard.tsx
+++ react/vanilla-projects/ervis-router/src/guards/auth.guard.tsx
@@ -1,11 +1,27 @@
 import { useSelector } from "react-redux";
-import  type { AppStore } from "../redux/store";
-import { PublicRoutes } from "../models";
+import type { AppStore } from "../redux/store";
+import { PrivateRoutes, PublicRoutes } from "../models";
 import { Navigate, Outlet } from "react-router-dom";
 
-export const AuthGuard = () => {
-    const userState = useSelector((store: AppStore) => store.user);
-    return userState.name ? <Outlet /> : <Navigate replace to={PublicRoutes.LOGIN} />;
+interface Props {
+  privateValidation: boolean;
 }
+
+const PrivateValidationFragment = <Outlet />;
+const PublicValidationFragment = <Navigate replace to={PrivateRoutes.PRIVATE} />;
+
+export const AuthGuard = ({ privateValidation }: Props) => {
+  const userState = useSelector((store: AppStore) => store.user);
+  
+  return userState.name ? (
+    privateValidation ? (
+      PrivateValidationFragment
+    ) : (
+      PublicValidationFragment
+    )
+  ) : (
+    <Navigate replace to={PublicRoutes.LOGIN} />
+  );
+};
 
 export default AuthGuard;
